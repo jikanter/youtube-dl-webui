574cd9bb3f0237b9db502efa55b8093e2852eef2
Jordan Kanter <jikanter@gmail.com>
2015-05-18 15:31:42 -0500

    fixed ssl issue with certain python versions, added a config section for static file host

--- youtube-download.js
+++ youtube-download.js
@@ -5,12 +5,28 @@ var parse = require('querystring').parse;
 var emitter = require('events').EventEmitter;
 var url = require('url');
 
+
+
+
+var config = {
+  pythonVersion: [2,7,9],
+  staticHost: "localhost"
+}
+
+
+
+
 http.createServer(function(request, response) { 
   
   var video = url.parse(request.url, true).query;
+  pythonVersion = config['pythonVersion'];
+  staticHost = config['staticHost'];
+  //  see https://github.com/rg3/youtube-dl/issues/4896
+  var checkSslCertificateP = ((pythonVersion[0] > 3) || ((pythonVersion[0] == 2) && (pythonVersion[2] < 9)));
   if (video.url) { 
     console.log(video.url);
     var ytDownloader = spawn('/usr/local/bin/youtube-dl', ['-o', '/usr/local/var/videos/%(id)s.flv',
+    checkSslCertificateP ? "" : "--no-check-certificate" ,
     '--exec', 'mv {} /usr/local/var/videos/youtube.flv',
     '-f', 'flv', video.url]);
     
@@ -40,7 +56,7 @@ http.createServer(function(request, response) {
       response.write('</section>');
       response.write('</div>');
       response.write('</form>');
-      response.write("<a href='http://it-www:9001/youtube.flv'>Download Video</a> <p>Directions: Right-Click &quot;Download Video&quot;, Then click Save-As to save the video to your desktop</p>");
+      response.write("<a href='http://"+staticHost+":9001/youtube.flv'>Download Video</a> <p>Directions: Right-Click &quot;Download Video&quot;, Then click Save-As to save the video to your desktop</p>");
       response.write('</body>');
       response.write('</html>');
       response.end();
