af9ffa48594b6c43f54e2e1555322504cf7b7946
Jordan Kanter <jikanter@gmail.com>
2017-09-27 02:00:56 -0500

    change default static path

--- static.js
+++ static.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('/usr/local/var/videos'));
+app.use('/', express.static('/Volumes/FRANKLIN/Music'));
 
 var server = app.listen(process.env.PORT || 9001, function() { 
   console.log('Listening on port %d', server.address().port);
